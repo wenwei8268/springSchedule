6c4489928ae2d7ab998d79be63adfc7b8b4df498
wenwei zww <wenweizww@wenweideMacBook-Pro.local>
2017-03-13 17:33:38 +0800

    renove unuse

--- src/main/java/com/johj/mail/JavaMail.java
+++ src/main/java/com/johj/mail/JavaMail.java
@@ -12,56 +12,7 @@ public class JavaMail {
 
     public static void main(String[] args) throws EmailException, MalformedURLException
     {
-//        JavaMail simpleMail = new JavaMail();
-//        simpleMail.sendTLS();
-//        try {
-//            MailSender mailSender = new MailSenderImpl();
-//            MailMessage mailMessage = new MailMessage();
-//
-//            mailMessage
-//                    .subject("hi，您有一封注册邮件！！！")
-//                    .from("support@wuxiapptec.com")
-//                    .content("hello worlds ")
-//                    .addTo("li_xia@wuxiapptec.com");
-//                    //.addFile("/Users/Anne/Documents/3849072.jpg", "/Users/Anne/Documents/vps.md");
-//
-//            mailSender.debug(true).host("10.111.5.158").username("zhou_wenwei@wuxiapptec.com").password("z.ww1234#");
-//
-//            //mailSender.host("10.111.5.158").;
-//            mailSender.send(mailMessage);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
 
-
-
-        MultiPartEmail email = new MultiPartEmail();
-
-        email.setHostName("10.111.5.158");
-//        email.setAuthentication("youremail@qq.com", "***");//邮件服务器验证：用户名/密码
-        email.setCharset("UTF-8");
-
-        email.setFrom("limssupport@wuxiapptec.com", "马化腾");
-        email.addTo("li_xia@wuxiapptec.com");
-
-        email.setSubject("加油奋斗！");
-        email.setMsg("come on! 加油奋斗吧，早晚有一天你会出任CEO，迎娶白富美，走上人生巅峰！ http://www.wuxinextcode.com\n\n\n pony ,请勿回复，谢谢");
-
-        EmailAttachment attachment = new EmailAttachment();
-        attachment.setPath("d:/lzl.jpg");// 本地文件
-        attachment.setDisposition(EmailAttachment.ATTACHMENT);
-        attachment.setDescription("林志玲1");
-        attachment.setName("lzl_1");
-        email.attach(attachment);
-
-//        EmailAttachment attachment2 = new EmailAttachment();
-//        attachment2.setURL(new URL("http://mat1.qq.com/datalib_img/star/pic/lib/2007-01-15/2007011511104716122311.jpg"));//远程文件
-//        attachment2.setDisposition(EmailAttachment.ATTACHMENT);
-//        attachment2.setDescription("林志玲2");
-//        attachment2.setName("lzl_2");
-//        email.attach(attachment2);
-
-        email.send();
 
     }
     public boolean sendMail(String fromMail,String toMail,String context){
