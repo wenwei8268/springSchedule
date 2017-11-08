2f6dd76b3df38b7c315d80db8b79339859ba4547
wenweizww@wenweideMBP <zhou_wenwei@wuxiapptec.com>
2017-11-08 10:10:52 +0800

    create xml

---
+++ src/main/java/com/johj/java9/TestInt.java
@@ -0,0 +1,16 @@
+package com.johj.java9;
+
+
+
+/**
+ * author:zhou_wenwei
+ * mail:
+ * date:2017/10/24
+ * description:
+ */
+public class TestInt {
+    public static void main(String[] args){
+        float i = 10.2f;
+        System.out.println(new Integer(10));
+    }
+}
